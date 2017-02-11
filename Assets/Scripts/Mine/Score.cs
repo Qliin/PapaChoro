15a54fef0d12dce16301e04fb0b3cebda033ebd2
HANNAH\user <h.costa.pinheiro@gmail.com>
2017-02-11 13:56:32 -0400

    Cholo

---
+++ Assets/Scripts/Mine/Score.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour {
+
+	private int currScore;
+	private int winScore = 15;
+	private Text scoreText;
+	public GameObject winTxt;
+	public GameObject gameOverTxt;
+	public GameObject restartBtn;
+
+	// Use this for initialization
+	void Start () {
+		currScore = 0;
+		scoreText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void AddToScore(){
+		++currScore;
+		scoreText.text = currScore.ToString ();
+		if (currScore == winScore) {
+			ShowGameOver (winTxt);
+//			winTxt.SetActive (true);
+//			Time.timeScale = 0;
+		}
+	}
+
+	void SubFromScore(){
+		--currScore;
+		scoreText.text = currScore.ToString ();
+		if (currScore < 0) {
+			ShowGameOver (gameOverTxt);
+//			gameOverTxt.SetActive (true);
+//			Time.timeScale = 0;
+		}
+	}
+
+	void ShowGameOver(GameObject textToShow) {
+		textToShow.SetActive (true);
+		restartBtn.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	public void RestartScene() {
+		Time.timeScale = 1;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
---
+++ Assets/Scripts/Mine/Tear.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tear : MonoBehaviour {
+
+	private GameObject score;
+	private bool hit = false;
+	// Use this for initialization
+	void Start () {
+		score = GameObject.FindGameObjectWithTag ("Score");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (!hit) {
+			hit = true;
+			if (col.gameObject.tag == "Boca") {
+				score.SendMessage ("AddToScore");
+			} else {
+				score.SendMessage ("SubFromScore");
+			}
+		}
+	}
+}
